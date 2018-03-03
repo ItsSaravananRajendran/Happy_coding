c3abc7017412f8a9113dbde2af916e57d19fd4d0
lidwi2win <lidwin2win@gmail.com>
2018-03-03 16:02:02 +0530

    C++_FEature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import os
 import sys
+import re
 import subprocess
 
 parent_dir = os.path.abspath(os.path.dirname(__file__))
@@ -21,24 +22,49 @@ class ExampleCommand(sublime_plugin.TextCommand):
 		file_name = file.split('.')
 		ext = file_name[-1]
 		if ext == 'py':
-			compiler = 'python'
+			compiler = 'python2'
 			args = [compiler,'-u',file]
 		elif ext == 'c':
 			compiler = 'gcc'
 			args = [compiler,file]
-		elif ext == 'c++':
+		elif ext == 'cpp':
 			compiler = 'g++'
 			args = [compiler,file]
 		child = subprocess.Popen(args=args,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-		output = str(child.communicate()[0])
+		output = str(child.communicate()[0].decode("UTF-8"))
 		rc = child.returncode
-		print (output)
+		#print (output)
 		if rc is not 0:
-			ind = output.index('^\\n')
-			errString = output[ind+3:]
-			errString = errString.replace('\\n\'',' ')
-			print (errString)
-			self.get_solution_stackoverflow(errString)
+			if compiler == "python2":
+				ind = output.index('^\\n')
+				errString = output[ind+3:]
+				errString = errString.replace('\\n\'',' ')
+				print (errString)
+				self.get_solution_stackoverflow(errString,compiler)
+			elif compiler == "g++":
+				print("a")
+				print(type(output))
+				print(output)
+				print(output.find("error:"))
+				starts = [m.end()+1 for m in re.finditer("error:",output)]
+				lis_of_err=[]
+				for error_end in starts:
+					str_t = ""
+					i = error_end
+					while True:
+						str_t = str_t + output[i]
+						i = i + 1
+						if output[i] == '\n':
+							break
+					str_t = str_t.replace('#','%23')
+					lis_of_err.append(str_t)
+
+				print(lis_of_err)
+
+				for errors in lis_of_err:
+					print(errors)
+					self.get_solution_stackoverflow(errors,compiler)
+
 		else:
 			text = str(output)
 			print ("rc = ",rc,"text =",text)
@@ -47,17 +73,26 @@ class ExampleCommand(sublime_plugin.TextCommand):
 		
 
 
-	def get_solution_stackoverflow(self,error_term):	
+	def get_solution_stackoverflow(self,error_term,compiler):	
+		temp_term = error_term
 		error_term = error_term.replace(' ','%20')
-		url = "https://api.stackexchange.com/2.2/search?page=1&pagesize=10&order=desc&sort=activity&tagged=python&intitle="+error_term+"&site=stackoverflow&filter=!Sm*O0f69(tqGyj3*s1"
+		url = "https://api.stackexchange.com/2.2/search?page=1&pagesize=10&order=desc&sort=activity&tagged="+compiler+"&intitle="+error_term+"&site=stackoverflow&filter=!Sm*O0f69(tqGyj3*s1"
+		print(url)
 		data = requests.get(url)
 		json = data.json()
+		if json["items"] == []:
+			#it means that stack overflow does not have an answer for this! thus i just append the original error
+			con = temp_term
+			self.create_phantoms(con)
+			return con
+
 		con =  str(json['items'][0]['answers'][0]['body'])
+		#print (con)
 		con = con.replace('\n','<br />')
 		con = con.replace('<pre>','<div class="preCode"><pre>')
 		con = con.replace('</pre>','</pre></div>')
 		self.create_phantoms(con)
-
+		return con
 
 	def style(self,content):
 		return content
@@ -86,7 +121,7 @@ class ExampleCommand(sublime_plugin.TextCommand):
 					   	</div>
 					   	
 				   	</body></html>'''
-		with open("/home/thunderbolt/out.html",'w') as file:
+		with open("/home/jessuva/out.html",'w') as file:
 			file.write(html)
 
 		
